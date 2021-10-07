1f7be534c124652ce615a41df0c2e80d71cdda4a
Max Wolter <awfm9@protonmail.com>
2021-10-07 13:30:15 +0200

    Update version tag on Coreth

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Set up the versions to be used
-coreth_version=${CORETH_VERSION:-'v0.1.0'}
+coreth_version=${CORETH_VERSION:-'v0.1.1'}
 # Don't export them as they're used in the context of other calls
 avalanche_version=${AVALANCHE_VERSION:-'v1.5.0-fuji'}
