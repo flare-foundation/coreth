51759a790da94e83025369b2b4fbf651654123f5
Max Wolter <awfm9@protonmail.com>
2021-12-19 13:43:37 +0000

    Enable Apricot Phases 3 and 4

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Set up the versions to be used
-coreth_version=${CORETH_VERSION:-'v0.1.6'}
+coreth_version=${CORETH_VERSION:-'v0.1.8'}
 # Don't export them as they're used in the context of other calls
-flare_version=${FLARE_VERSION:-'v0.2.3'}
+flare_version=${FLARE_VERSION:-'v0.3.4'}
