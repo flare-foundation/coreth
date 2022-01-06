4b0196625f40ea39abbcc1046063a1fa957c185f
Satyajit <emailtovamos@gmail.com>
2022-01-06 15:57:58 +0000

    Unpolished evm call that works as expected with some todos

--- plugin/evm/flare.go
+++ plugin/evm/flare.go
@@ -6,6 +6,9 @@ package evm
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/flare-foundation/coreth/params"
+	"math/big"
 	"net/http"
 	"time"
 
@@ -13,7 +16,6 @@ import (
 	"github.com/flare-foundation/coreth/core"
 	"github.com/flare-foundation/coreth/core/types"
 	"github.com/flare-foundation/coreth/core/vm"
-	"github.com/flare-foundation/flare/vms/proposervm/block"
 )
 
 type Contract interface {
@@ -33,7 +35,7 @@ type FlareAPI struct {
 }
 
 func (f *FlareAPI) GetCreators(_ *http.Request, req *GetCreatorsRequest, res *GetCreatorsResponse) error {
-
+	log.Info("GetCreators called...")
 	data := []byte{
 		// 4 bytes function code
 	}
@@ -44,7 +46,7 @@ func (f *FlareAPI) GetCreators(_ *http.Request, req *GetCreatorsRequest, res *Ge
 		0,                 // nonce,
 		nil,               // amount
 		0,                 // gaslimit
-		nil,               // gasprice
+		big.NewInt(5),     // gasprice
 		nil,               // gasfeecap
 		nil,               // gastipcap
 		data,              // data
@@ -58,17 +60,32 @@ func (f *FlareAPI) GetCreators(_ *http.Request, req *GetCreatorsRequest, res *Ge
 	}
 
 	tx := core.NewEVMTxContext(msg)
-	block := core.NewEVMBlockContext(block.Header(), f.blockchain, nil)
-	evm := vm.NewEVM(block, tx, state, nil, vm.Config{})
-
-	creatorsByte, _, err := evm.Call(nil, getCreatorsContractAddress(), getCreatorsContractFunction4Bytes(), 0, nil)
+	header := types.Header{
+		BaseFee: nil,
+		Number:  big.NewInt(1), //todo currently block height is 1. todo: api call needs to give us the block number we care about
+		// block number and hash.
+		Difficulty: big.NewInt(1),
+	}
+	//block := core.NewEVMBlockContext(block.Header(), f.blockchain, nil)
+	block := core.NewEVMBlockContext(&header, f.blockchain, nil)
+	chainConfig := params.ChainConfig{
+		ChainID:             big.NewInt(4294967295),
+		ByzantiumBlock:      big.NewInt(0),
+		ConstantinopleBlock: big.NewInt(0),
+	}
+	evm := vm.NewEVM(block, tx, state, &chainConfig, vm.Config{})
+	evmCallValue := big.NewInt(0)
+	//evmCallValue = nil
+	caller := vm.AccountRef(getCreatorsContractAddress())
+	creatorsByte, _, err := evm.Call(caller, getCreatorsContractAddress(), getValidatorsContractFunction4Bytes(), 100000, evmCallValue)
 	//caller ContractRef, addr common.Address, input []byte, gas uint64, value *big.Int
 	if err != nil {
 		return fmt.Errorf("could not get block creators from contract: %w", err)
 	}
-
+	log.Info("result: ", "result: ", fmt.Sprintf("%x", creatorsByte))
 	creators := make(map[string]uint64)
 	err = json.Unmarshal(creatorsByte, &creators)
+	log.Info("creatorsByte..: ", "len(creatorsByte)", creatorsByte)
 	if err != nil {
 		return fmt.Errorf("unmarshalling error while trying to get block creators from contract: %w", err)
 	}
@@ -88,3 +105,12 @@ func getCreatorsContractFunction4Bytes() []byte {
 		return []byte{0xe6, 0xad, 0xc1, 0xee} //getCreators()
 	}
 }
+
+func getValidatorsContractFunction4Bytes() []byte {
+	switch {
+	default:
+		return []byte{0xb7, 0xab, 0x4d, 0xb5} //getValidators()
+	}
+}
+
+//getValidators() b7 ab 4d b5
