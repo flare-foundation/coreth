641492cd07180c4d99e66b1ee2c96c398981bb8a
Satyajit <emailtovamos@gmail.com>
2022-01-20 12:40:46 +0000

    Change version of Flare

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -3,4 +3,4 @@
 # Set up the versions to be used
 coreth_version=${CORETH_VERSION:-'v0.2.1'}
 # Don't export them as they're used in the context of other calls
-flare_version=${FLARE_VERSION:-'v0.4.0'}
+flare_version=${FLARE_VERSION:-'f19fe94ba4ef45e8ac7b31877471162b65ee6f43'}
