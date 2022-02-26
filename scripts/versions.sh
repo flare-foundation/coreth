9f09181ae5d59921adac6c616fe10a2ccd138994
Max Wolter <awfm9@protonmail.com>
2022-02-26 18:30:38 +0000

    Fix EVM version

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 # Set up the versions to be used
-coreth_version=${CORETH_VERSION:-'v0.3.2'}
-# Don't export them as they're used in the context of other calls
-flare_version=${FLARE_VERSION:-'v0.5.1'}
+coreth_version=${CORETH_VERSION:-'v0.3.3'}
