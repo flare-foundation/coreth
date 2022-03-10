7cb5afcd84ddbfd498c42a32cd308afd88d3fe60
Max Wolter <awfm9@protonmail.com>
2022-03-10 18:01:42 +0000

    Add retrieval for epoch data providers

--- plugin/evm/ftso.go
+++ plugin/evm/ftso.go
@@ -24,10 +24,13 @@ const (
 )
 
 type FTSO struct {
-	blockchain *core.BlockChain
-	last       uint64
-	seconds    uint64
-	epochs     *lru.ARCCache
+	blockchain  *core.BlockChain
+	last        uint64
+	seconds     uint64
+	epochs      *lru.ARCCache
+	manager     common.Address
+	registry    common.Address
+	whitelister common.Address
 }
 
 func NewFTSO(blockchain *core.BlockChain) (*FTSO, error) {
@@ -39,6 +42,27 @@ func NewFTSO(blockchain *core.BlockChain) (*FTSO, error) {
 		last:       0,
 		epochs:     epochs,
 	}
+
+	manager, err := f.FTSOManager()
+	if err != nil {
+		return nil, fmt.Errorf("could not get FTSO manager address: %w", err)
+	}
+
+	f.manager = manager
+
+	registry, err := f.FTSORegistry()
+	if err != nil {
+		return nil, fmt.Errorf("could not get FTSO registry: %w", err)
+	}
+
+	f.registry = registry
+
+	whitelister, err := f.VoterWhitelister()
+	if err != nil {
+		return nil, fmt.Errorf("could not get voter whitelister: %w", err)
+	}
+
+	f.whitelister = whitelister
 
 	seconds, err := f.RewardEpochDurationSeconds()
 	if err != nil {
@@ -77,7 +101,40 @@ func (f *FTSO) EpochForTimestamp(timestamp uint64) (uint64, error) {
 }
 
 func (f *FTSO) ProvidersForEpoch(epoch uint64) ([]common.Address, error) {
-	return nil, nil
+
+	info, err := f.RewardEpochs(epoch)
+	if err != nil {
+		return nil, fmt.Errorf("could not get rewards epoch: %w", err)
+	}
+
+	header := f.blockchain.GetHeaderByNumber(info.block)
+	if header == nil {
+		return nil, fmt.Errorf("unknown header (number: %d)", info.block)
+	}
+
+	hash := header.Hash()
+	indices, err := f.GetSupportedIndices(hash)
+	if err != nil {
+		return nil, fmt.Errorf("could not get FTSOs: %w", err)
+	}
+
+	providerSet := make(map[common.Address]struct{})
+	for _, index := range indices {
+		addresses, err := f.GetFTSOWhitelistedPriceProviders(hash, index)
+		if err != nil {
+			return nil, fmt.Errorf("could not get whitelisted price providers (index: %d): %w", index, err)
+		}
+		for _, address := range addresses {
+			providerSet[address] = struct{}{}
+		}
+	}
+
+	providers := make([]common.Address, 0, len(providerSet))
+	for provider := range providerSet {
+		providers = append(providers, provider)
+	}
+
+	return providers, nil
 }
 
 func (f *FTSO) ValidatorForProviderAtEpoch(epoch uint64, provider common.Address) (ids.ShortID, error) {
@@ -92,9 +149,23 @@ func (f *FTSO) RewardForProviderAtEpoch(epoch uint64, provider common.Address) (
 	return 0, nil
 }
 
+func (f *FTSO) FTSOManager() (common.Address, error) {
+	return common.Address{}, nil
+}
+
+func (f *FTSO) FTSORegistry() (common.Address, error) {
+	return common.Address{}, nil
+}
+
+func (f *FTSO) VoterWhitelister() (common.Address, error) {
+	return common.Address{}, nil
+}
+
 func (f *FTSO) RewardEpochDurationSeconds() (uint64, error) {
 
-	values, err := f.call(f.blockchain.CurrentHeader().Hash(), ftsoManager, "rewardEpochDurationSeconds")
+	hash := f.blockchain.CurrentHeader().Hash()
+
+	values, err := f.call(hash, f.manager, ftsoManagerABI, "rewardEpochDurationSeconds")
 	if err != nil {
 		return 0, fmt.Errorf("could not get reward epoch duration seconds: %w", err)
 	}
@@ -108,6 +179,7 @@ func (f *FTSO) RewardEpochDurationSeconds() (uint64, error) {
 }
 
 type rewardEpochs struct {
+	block uint64
 	start uint64
 	end   uint64
 }
@@ -119,17 +191,21 @@ func (f *FTSO) RewardEpochs(epoch uint64) (rewardEpochs, error) {
 		return entry.(rewardEpochs), nil
 	}
 
-	values, err := f.call(f.blockchain.CurrentHeader().Hash(), ftsoManager, "rewardEpochs", big.NewInt(0).SetUint64(epoch))
+	hash := f.blockchain.CurrentHeader().Hash()
+
+	values, err := f.call(hash, f.manager, ftsoManagerABI, "rewardEpochs", big.NewInt(0).SetUint64(epoch))
 	if err != nil {
 		return rewardEpochs{}, fmt.Errorf("could not get reward epochs: %w", err)
 	}
 	if len(values) != 3 {
-		return rewardEpochs{}, fmt.Errorf("wrong number of return values  (have: %d, want: %d)", len(values), 3)
+		return rewardEpochs{}, fmt.Errorf("wrong number of return values (have: %d, want: %d)", len(values), 3)
 	}
 
+	block := values[0].(*big.Int).Uint64()
 	start := values[2].(*big.Int).Uint64()
 
 	info := rewardEpochs{
+		block: block,
 		start: start,
 		end:   start + f.seconds,
 	}
@@ -139,7 +215,37 @@ func (f *FTSO) RewardEpochs(epoch uint64) (rewardEpochs, error) {
 	return info, nil
 }
 
-func (f *FTSO) call(hash common.Hash, abi abi.ABI, method string, params ...interface{}) ([]interface{}, error) {
+func (f *FTSO) GetSupportedIndices(hash common.Hash) ([]*big.Int, error) {
+
+	values, err := f.call(hash, f.registry, ftsoRegistryABI, "getSupportedIndices")
+	if err != nil {
+		return nil, fmt.Errorf("could not get FTSO indices: %w", err)
+	}
+	if len(values) != 1 {
+		return nil, fmt.Errorf("wrong number of return values (have %d, want: %d)", len(values), 1)
+	}
+
+	indices := values[0].([]*big.Int)
+
+	return indices, nil
+}
+
+func (f *FTSO) GetFTSOWhitelistedPriceProviders(hash common.Hash, index *big.Int) ([]common.Address, error) {
+
+	values, err := f.call(hash, f.whitelister, voterWhitelisterABI, "getFtsoWhitelistedPriceProviders", index)
+	if err != nil {
+		return nil, fmt.Errorf("could not get whitelisted price providers: %w", err)
+	}
+	if len(values) != 1 {
+		return nil, fmt.Errorf("wrong number of return values (have %d, want: %d)", len(values), 1)
+	}
+
+	addresses := values[0].([]common.Address)
+
+	return addresses, nil
+}
+
+func (f *FTSO) call(hash common.Hash, to common.Address, abi abi.ABI, method string, params ...interface{}) ([]interface{}, error) {
 
 	data, err := abi.Pack(method, params...)
 	if err != nil {
@@ -157,7 +263,7 @@ func (f *FTSO) call(hash common.Hash, abi abi.ABI, method string, params ...inte
 	}
 
 	input := hexutil.Bytes(data)
-	args := ethapi.TransactionArgs{Input: &input}
+	args := ethapi.TransactionArgs{To: &to, Input: &input}
 	msg, err := args.ToMessage(0, nil)
 	if err != nil {
 		return nil, fmt.Errorf("could not convert arguments to message: %w", err)
