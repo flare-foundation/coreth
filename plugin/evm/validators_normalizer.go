d46888c07331cd7e864968bac40947545c658688
Max Wolter <awfm9@protonmail.com>
2022-03-19 14:25:05 +0000

    Fix default validators weights (#18)

--- plugin/evm/validators_normalizer.go
+++ plugin/evm/validators_normalizer.go
@@ -10,10 +10,15 @@ import (
 	"github.com/flare-foundation/flare/ids"
 )
 
+// ValidatorsNormalizer is responsible for normalizing validators, so that their
+// weights come out to the same total, irrespective of their original weights or
+// how many validators are in a set.
 type ValidatorsNormalizer struct {
 	validators ValidatorRetriever
 }
 
+// NewValidatorsNormalizer wraps a new validators retriever in the normalizer, making
+// sure that all sets retrieved from the wrapper retriever have the same total weight.
 func NewValidatorsNormalizer(validators ValidatorRetriever) *ValidatorsNormalizer {
 
 	v := ValidatorsNormalizer{
@@ -23,6 +28,8 @@ func NewValidatorsNormalizer(validators ValidatorRetriever) *ValidatorsNormalize
 	return &v
 }
 
+// ByEpoch retrieves the validators from the underlying retriever and normalizes
+// their weights so that they always have the same total approximate weight.
 func (v *ValidatorsNormalizer) ByEpoch(epoch uint64) (map[ids.ShortID]uint64, error) {
 
 	validators, err := v.validators.ByEpoch(epoch)
