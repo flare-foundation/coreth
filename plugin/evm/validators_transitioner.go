cecf9f7dc7840eeadffbff1347e5070909e56f44
Max Wolter <awfm9@protonmail.com>
2022-03-19 14:22:17 +0000

    Fix default validators weights

--- plugin/evm/validators_transitioner.go
+++ plugin/evm/validators_transitioner.go
@@ -4,7 +4,9 @@
 package evm
 
 import (
+	"bytes"
 	"fmt"
+	"sort"
 
 	"github.com/flare-foundation/flare/ids"
 )
@@ -27,16 +29,17 @@ func WithMinSteps(steps uint) TransitionOption {
 	}
 }
 
-// ValidatorsTransitioner transitions a set of validators from one retriever to
-// the second retriever in a smooth deterministic manner over a pre-configured
-// minimum number of steps.
+// ValidatorsTransitioner transitions validators from a static set of validators
+// to a growing set of dynamic validators over a number of smooth steps.
 type ValidatorsTransitioner struct {
-	validators []ids.ShortID
+	validators map[ids.ShortID]uint64
 	providers  ValidatorRetriever
 	cfg        TransitionConfig
 }
 
-func NewValidatorsTransitioner(validators []ids.ShortID, providers ValidatorRetriever, opts ...TransitionOption) *ValidatorsTransitioner {
+// NewValidatorsTransitioner creates a transition from the given validators to the
+// validators retrieved from the given providers.
+func NewValidatorsTransitioner(validators map[ids.ShortID]uint64, providers ValidatorRetriever, opts ...TransitionOption) *ValidatorsTransitioner {
 
 	cfg := DefaultTransitionConfig
 	for _, opt := range opts {
@@ -53,13 +56,27 @@ func NewValidatorsTransitioner(validators []ids.ShortID, providers ValidatorRetr
 
 }
 
+// ByEpoch returns a set of validators that is given by a smooth transition from an
+// original static set of validators, to a new dynamic set of validators that is
+// growing and continuously takes up more of the total set until the original static
+// validators have been entirely phased out.
 func (v *ValidatorsTransitioner) ByEpoch(epoch uint64) (map[ids.ShortID]uint64, error) {
 
+	// First, we get the dynamic set of validators from the underlying retriever.
 	providers, err := v.providers.ByEpoch(epoch)
 	if err != nil {
-		return nil, fmt.Errorf("could not get provider: %w", err)
+		return nil, fmt.Errorf("could not get provider validators: %w", err)
 	}
 
+	// If there are non, we always return the full set of static validators.
+	if len(providers) == 0 {
+		return v.validators, nil
+	}
+
+	// Otherwise, we try to identify the number of the step we are at in our transition
+	// from the static to the dynamic set. It can range from zero to the minimum number
+	// of steps; at zero, we still return the static set; at minimum steps reached, we
+	// return only the dynamic set.
 	size := uint(len(v.validators))
 	steps := uint(0)
 Loop:
@@ -67,6 +84,9 @@ Loop:
 		thresholds := findThresholds(size, try, v.cfg.MinSteps)
 		for i, threshold := range thresholds {
 			e := epoch - uint64(i)
+			if e > epoch {
+				break Loop
+			}
 			providers, err := v.providers.ByEpoch(e)
 			if err != nil {
 				return nil, fmt.Errorf("could not get providers: %w", err)
@@ -78,19 +98,48 @@ Loop:
 		}
 	}
 
-	cutoff := (size - steps*size/v.cfg.MinSteps)
-	validators := v.validators[:cutoff]
+	// If we are not ready to take any steps yet, we stick with the default
+	// validator set still.
+	if steps == 0 {
+		return v.validators, nil
+	}
 
-	totalWeight := uint64(0)
+	// If we have reached the minimum number of steps, we can return the dynamic
+	// set.
+	if steps == v.cfg.MinSteps {
+		return providers, nil
+	}
+
+	// If we are somewhere in-between, we need to balance the list of providers
+	// with the list of static default validators. First, we choose a deterministic
+	// list of validator IDs from the default validators, cut off at the appropriate
+	// percentage depending on steps
+	validators := make([]ids.ShortID, 0, len(v.validators))
+	for validator := range v.validators {
+		validators = append(validators, validator)
+	}
+	sort.Slice(validators, func(i int, j int) bool {
+		return bytes.Compare(validators[i][:], validators[j][:]) < 0
+	})
+	cutoff := (size - steps*size/v.cfg.MinSteps)
+	validators = validators[:cutoff]
+
+	// Then, we try to balance the weights between the default validators and the
+	// validators from the FTSO. In order to do so, we calculate the total weight
+	// of providers. From that, we derive the total weigth we should have based on
+	// the step we are on and the proportion the providers should have. Then we
+	// derive the weight default validators should have based on the total.
+	providerWeight := uint64(0)
 	for _, weight := range providers {
-		totalWeight += weight
+		providerWeight += weight
 	}
-	averageWeight := totalWeight / uint64(size)
-	if averageWeight == 0 {
-		averageWeight = v.cfg.Placeholder
-	}
+	totalWeight := (providerWeight) * uint64(v.cfg.MinSteps) / uint64(steps)
+	validatorWeight := totalWeight * uint64(v.cfg.MinSteps-steps) / uint64(v.cfg.MinSteps)
+
+	// Finally, we add the selected default validators to the set of the providers
+	// with the weights we have calculated.
 	for _, validator := range validators {
-		providers[validator] = averageWeight
+		providers[validator] = validatorWeight
 	}
 
 	return providers, nil
