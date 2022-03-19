80b35db6e9993d6aa64158d05259fe3c165fb0bb
Bogdan Prodan <bogdan.prodan.j@gmail.com>
2022-03-20 01:43:57 +0200

    Added integration tests for evm calls (#14)
    * Added evm_call tests
    
    * Added mocks for smart contracts
    
    * Fixed comments
    
    * WIP common case for ftso_system
    
    * Finished test cases for Contracts
    
    * Fixed comments
    
    * Fixed typo in file name

---
+++ plugin/evm/evm_call_test.go
@@ -0,0 +1,63 @@
+//go:build integration
+// +build integration
+
+package evm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEVMCall_Execute(t *testing.T) {
+
+	initTestContracts(t)
+
+	auth, be := simulatedBlockchain(t)
+	defer be.Close()
+
+	contractAddr := deployTestContract(t, auth, be, testAbiStore, testStoreBin)
+
+	be.Commit(true)
+
+	contract := EVMContract{
+		address: contractAddr,
+		abi:     testAbiStore,
+	}
+
+	blockChain := be.Blockchain()
+
+	evmBind := BindEVM(blockChain)
+
+	t.Run("nominal case", func(t *testing.T) {
+		header := blockChain.LastAcceptedBlock().Header()
+
+		ret := evmBind.AtBlock(header.Hash()).OnContract(contract).Execute("setItem", [32]byte{1}, [32]byte{1})
+		require.NoError(t, ret.err)
+
+		ret = evmBind.AtBlock(header.Hash()).OnContract(contract).Execute("items", [32]byte{1})
+		require.NoError(t, ret.err)
+		require.Len(t, ret.values, 1)
+	})
+
+	t.Run("handles invalid method", func(t *testing.T) {
+		header := blockChain.LastAcceptedBlock().Header()
+		ret := evmBind.AtBlock(header.Hash()).OnContract(contract).Execute("setItem2", [32]byte{1}, [32]byte{1})
+		assert.EqualError(t, ret.err, "could not pack parameters: method 'setItem2' not found")
+	})
+
+	t.Run("handles invalid parameters", func(t *testing.T) {
+		header := blockChain.LastAcceptedBlock().Header()
+		ret := evmBind.AtBlock(header.Hash()).OnContract(contract).Execute("setItem", [32]byte{1}, [32]byte{1}, [32]byte{1})
+		assert.EqualError(t, ret.err, "could not pack parameters: argument count mismatch: got 3 for 2")
+	})
+
+	t.Run("handles invalid header number", func(t *testing.T) {
+		header := blockChain.GetHeaderByNumber(2000)
+		hash := header.Hash()
+		ret := evmBind.AtBlock(hash).OnContract(contract).Execute("setItem", [32]byte{1}, [32]byte{1}, [32]byte{1})
+		assert.EqualError(t, ret.err, fmt.Sprintf("unknown block (hash: %x)", hash))
+	})
+}
