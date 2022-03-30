523e2de57b39bcc5c19cdcde1b4dadeab1cb4b9f
Max Wolter <awfm9@protonmail.com>
2022-03-30 17:47:32 +0100

    Add additional logging to hard fork updates

--- consensus/misc/contract_update.go
+++ consensus/misc/contract_update.go
@@ -6,6 +6,7 @@ import (
 	"bytes"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/log"
 
 	"github.com/flare-foundation/coreth/core/state"
 )
@@ -17,11 +18,27 @@ type CoreContractUpdate struct {
 }
 
 func updateApplied(statedb *state.StateDB, update CoreContractUpdate) bool {
+
+	code := statedb.GetCode(update.Address)
+
+	log.Debug("applied check: %s\n", update.Address)
+	log.Debug("new code:\n%x\n", update.Old)
+	log.Debug("actual code:\n%x\n", code)
+
 	return bytes.Equal(statedb.GetCode(update.Address), update.New)
 }
 
 func applyUpdate(statedb *state.StateDB, update CoreContractUpdate) {
+
+	code := statedb.GetCode(update.Address)
+
+	log.Debug("apply update: %s\n", update.Address)
+	log.Debug("current code:\n%x\n", update.Old)
+	log.Debug("old code:\n%x\n", update.New)
+	log.Debug("new code:\n%x\n", code)
+
 	if bytes.Equal(statedb.GetCode(update.Address), update.Old) {
+		log.Debug("applying update!")
 		statedb.SetCode(update.Address, update.New)
 	}
 }
